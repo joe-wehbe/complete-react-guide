45166f228b1009f231e6f9120ecf009d678aa960
Joe-Wehbe <joewehbe@yahoo.com>
2025-05-19 16:04:51 +0300

    02-react-essentials-deep-dive (s4-69) <> Modified projects

--- 01-react-essentials/src/components/Header/Header.jsx
+++ 01-react-essentials/src/components/Header/Header.jsx
@@ -8,14 +8,15 @@ function genRandomInt(max) {
 }
 
 export default function Header() {
-  const description = reactDescriptions[genRandomInt(2)]
+  const description = reactDescriptions[genRandomInt(2)];
 
   return (
     <header>
       <img src={reactImg} alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
-        {description} React concepts you will need for almost any app you are going to build!
+        {description} React concepts you will need for almost any app you are
+        going to build!
       </p>
     </header>
   );
