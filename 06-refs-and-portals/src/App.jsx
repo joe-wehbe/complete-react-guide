b586edb76e2da66ce354645446740f984c9d71c8
Joe Wehbe <joefwehbe@gmail.com>
2026-05-10 21:15:15 +0300

    Updated App.jsx

--- 06-refs-and-portals/src/App.jsx
+++ 06-refs-and-portals/src/App.jsx
@@ -2,7 +2,7 @@ import Player from './components/Player.jsx';
 import TimerChallenge from './components/TimerChallenge.jsx';
 
 function App() {
-  console.log("Hello");
+  console.log("Hellodad");
   return (
     <>
       <Player />
